1fc70e620e7c5f94b0d5330b28a1ad9fa257f589
Daniel Armengod <daniel@armengod.cat>
2020-02-03 19:45:49 +0100

    Alarm scheduling logic pretty much done.

--- main.py
+++ main.py
@@ -1,38 +1,57 @@
+import RPIO
 import subprocess
 import sys
+import time
 from bisect import bisect_left
+from threading import Timer
 
-import RPIO
+entrance_auth = 0.000
+door_toolong_timer = None
+RFID_READER_LINE_LEN = 10  # TODO verify this
 
 
 def index(container, x):
-    'Locate the leftmost value exactly equal to x'
+    """Locate the leftmost value exactly equal to x"""
     i = bisect_left(container, x)
     if i != len(container) and container[i] == x:
         return i
     return -1
 
 
-entrance_auth = 0
-RFID_READER_LINE_LEN = 10  # TODO verify this
-
-
 def play_alarm():
     subprocess.run('./alarm.sh')
 
 
-def door_callback(gpio_id, val):
+def door_open_callback(gpio_id, val):
     """
-    Called when the door is opened. Start alert playback if entrance isn't authorized.
+    Start alert playback if entrance isn't authorized.
     :param gpio_id:
     :param val:
     :return:
     """
-    # Both parameters unused
+    # Both parameters unused.
+    del gpio_id, val
+    if time.monotonic() < entrance_auth:
+        play_unauth_alarm()
+    # Set a timer to sound an alarm if the door stays open too long.
+    global door_toolong_timer
+    door_toolong_timer = Timer(30.000, play_toolong_alarm)
+
+
+def door_close_callback(gpio_id, val):
+    """
+    Reset door open authorization, stop any active alarm.
+    :param gpio_id:
+    :param val:
+    :return:
+    """
+    # Both parameters unused.
     del gpio_id, val
     global entrance_auth
-    if not entrance_auth:
-        play_alarm()
+    entrance_auth = 0.000
+    # Un-schedule "door open for too long" alarm
+    door_toolong_timer.cancel()
+    stop_alarms()
 
 
 def main():
@@ -54,4 +73,7 @@ def main():
             # invalid card # found.
             print(f'Invalid card # found: {new_id}')
             continue
+        global entrance_auth
+        entrance_auth = time.monotonic() + 60.000  # Grant door authorization for the next minute.
+        stop_alarms()
 
