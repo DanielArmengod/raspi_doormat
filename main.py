64ef599406aacaba768836ef0f7de60a5fbbdc15
Daniel Armengod <d.armengod@gmail.com>
2020-02-06 23:41:29 +0100

    Changed GPIO library

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
-import threading
-
-import RPIO
 import subprocess
 import sys
 import time
 from bisect import bisect_left
 from threading import Timer
 
+import gpiozero
+
 entrance_auth = 0.000
-RFID_READER_LINE_LEN = 10  # TODO verify this
+RFID_READER_LINE_LEN = 10  # TODO verify this  - 9 digits + \n
 alarm_process = None
 too_long_alarm_timer = None
 door_open = 0
+door_sense_gpio = None  # Will be defined as Button in main()
+
 
 def index(container, x):
     """Locate the leftmost value exactly equal to x"""
@@ -23,35 +24,36 @@ def index(container, x):
 
 def play_unauth_alarm():
     global alarm_process
-    alarm_process = subprocess.Popen(['cvlc', '-L', 'buzzer.mp3'])
+    print("play_unauth_alarm")
+    alarm_process = subprocess.Popen(['sudo', '-ufiberparty', 'cvlc', '-L', 'buzzer.mp3'])
 
 
 def play_toolong_alarm():
     # TODO: different sounds for different alarms
     global alarm_process
-    alarm_process = subprocess.Popen(['cvlc', '-L', 'buzzer.mp3'])
+    print("play_toolong_alarm")
+    alarm_process = subprocess.Popen(['sudo', '-ufiberparty', 'cvlc', '-L', 'buzzer.mp3'])
 
 
 def stop_alarms():
+    print("stop_alarms")
     global alarm_process
     try:
-        alarm_process.terminate()
-    except AttributeError:
+        alarm_process.send_signal(15)
+        alarm_process.wait()
+    except AttributeError as e:
+        print(e)
         pass
     alarm_process = None
 
 
-def door_open_callback(gpio_id, val):
+def door_open_handler():
     """
     Start alert playback if entrance isn't authorized.
-    :param gpio_id:
-    :param val:
-    :return:
     """
+    print("door_open_handler")
     global door_open
     door_open = 0
-    # Both parameters unused.
-    del gpio_id, val
     # If door opening was unauth'd, sound alarm and return.
     if time.monotonic() < entrance_auth:
         play_unauth_alarm()
@@ -61,20 +63,17 @@ def door_open_callback(gpio_id, val):
     if isinstance(too_long_alarm_timer, Timer):
         too_long_alarm_timer.cancel()
         too_long_alarm_timer = None
-    too_long_alarm_timer = Timer(45.000, play_toolong_alarm)
+    too_long_alarm_timer = Timer(5.000, play_toolong_alarm)
+    too_long_alarm_timer.start()
 
 
-def door_close_callback(gpio_id, val):
+def door_close_handler():
     """
     Reset door open authorization & stop any active alarm.
-    :param gpio_id:
-    :param val:
-    :return:
     """
+    print("door_close_handler")
     global door_open
     door_open = 0
-    # Both parameters unused.
-    del gpio_id, val
     # Deauthorize door.
     global entrance_auth
     entrance_auth = 0.000
@@ -88,9 +87,22 @@ def door_close_callback(gpio_id, val):
 
 
 def main():
+    # Configure door-sensing GPIO.
+    global door_sense_gpio
+    # GND pin is #39, bottom left of the pin header.
+    door_sense_gpio = gpiozero.Button(
+        21,  # Physical GPIO #40, bottom right near USB connectors.
+        pull_up=True,  # pull_up=False would mean "enable pull-down resistor", pull_up=None is "leave it floating".
+        bounce_time=0.20,
+        hold_time=0
+        )
+    # TODO: VERIFY LOGIC -- This logic is valid for Normally Open door-sense switch.
+    door_sense_gpio.when_pressed = door_open_handler
+    door_sense_gpio.when_released = door_close_handler
+
     # Populate the valid ID list.
     # Assumptions: list contains only legal strings (numbers), list is already ordered.
-    with open('./valid_ids.txt') as valid_ids_file:
+    with open('./valid-ids.txt') as valid_ids_file:
         valid_ids = list(map(int, valid_ids_file))
 
     # Main, endless loop.
@@ -114,8 +126,13 @@ def main():
                 too_long_alarm_timer.cancel()
                 too_long_alarm_timer = None
             too_long_alarm_timer = Timer(45.000, play_toolong_alarm)
+            too_long_alarm_timer.start()
             stop_alarms()
             continue
         # Grant door authorization & stop any active alarms.
         global entrance_auth
         entrance_auth = time.monotonic() + 20.000
+
+
+
+main()
